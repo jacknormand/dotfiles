f974e03752eccc424094af749381c82d6cd6325a
jacknormand <jack.normand15@gmail.com>
2024-03-06 12:38:20 -0800

    Completed for real this time
    probably not. fixed up startup screen

--- nvim/.config/nvim/lua/jackynorm/plugins/alpha.lua
+++ nvim/.config/nvim/lua/jackynorm/plugins/alpha.lua
@@ -37,10 +37,9 @@ return {
         dashboard.section.buttons.val = {
           -- dashboard.button("SPC j", "󰈚   Restore Session", ":SessionRestore<cr>"),
           dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
-          dashboard.button("f", "   Find file", ":cd $HOME/dotfiles/.config | Telescope find_files<CR>"),
+          dashboard.button("f", "   Find config file", ":cd $HOME/dotfiles/nvim/.config | Telescope find_files<CR>"),
           dashboard.button("g", "   Find word", ":Telescope live_grep<CR>"),
           dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
-          dashboard.button("c", "   Config", ":e $MYVIMRC <CR>"),
           dashboard.button("l", "   Lazy", ":Lazy<CR>"),
           dashboard.button("q", "   Quit NVIM", ":qa<CR>"),
         }    
